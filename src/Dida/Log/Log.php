dfd74c194d444850c1fe0d5212cf5ef6ae3e8fb8
Macc Liu <maccliu@qq.com>
2019-12-23 17:04:03 +0800

    重构

--- src/Dida/Log/Log.php
+++ src/Dida/Log/Log.php
@@ -1,75 +1,244 @@
 <?php
 /**
- * Dida Framework  -- A Rapid Development Framework
- * Copyright (c) Zeupin LLC. (http://zeupin.com)
+ * Dida Framework  -- PHP轻量级快速开发框架
+ * 版权所有 (c) 上海宙品信息科技有限公司
  *
- * Licensed under The MIT License.
- * Redistributions of files must retain the above copyright notice.
+ * 官网: <https://github.com/zeupin/dida>
+ * Gitee: <https://gitee.com/zeupin/dida>
  */
-
 namespace Dida\Log;
 
+use \Psr\Log\InvalidArgumentException;
+use \Psr\Log\LoggerInterface;
+
+/**
+ * Log
+ */
 class Log
 {
-    const VERSION = '20180508';
+    /**
+     * 版本号
+     */
+    const VERSION = '20191223';
 
-    public static $dir = null;
+    /**
+     * 处理函数
+     *
+     * @var array
+     */
+    protected static $handlers = [
+        'emergency' => null,
+        'alert'     => null,
+        'critical'  => null,
+        'error'     => null,
+        'warning'   => null,
+        'notice'    => null,
+        'info'      => null,
+        'debug'     => null,
+    ];
+
+    /**
+     * 日志实例
+     *
+     * @var LoggerInterface
+     */
+    protected static $logger = null;
 
 
-    public static function write($var)
+    /**
+     * 初始化
+     *
+     * @param array $settings   设置项数组
+     * [
+     *     "classname" => Logger类的类名, // 此日志类需要实现 \Psr\Log\LoggerInterface 接口
+     * ]
+     */
+    public static function init(array $settings)
     {
-        $num_args = func_num_args();
-        if (!$num_args) {
-            return false;
+        // 对classname的处理
+        if (array_key_exists("classname", $settings)) {
+            // 生成类实例
+            static::$logger = new $settings["classname"];
+            // 初始化类实例
+            static::$logger->init($settings);
+            // 生成callback
+            static::$handlers = [
+                'emergency' => [static::$logger, "emergency"],
+                'alert'     => [static::$logger, "alert"],
+                'critical'  => [static::$logger, "critical"],
+                'error'     => [static::$logger, "error"],
+                'warning'   => [static::$logger, "warning"],
+                'notice'    => [static::$logger, "notice"],
+                'info'      => [static::$logger, "info"],
+                'debug'     => [static::$logger, "debug"],
+            ];
         }
-
-        if (self::$dir === null || !file_exists(self::$dir) || !is_dir(self::$dir)) {
-            return false;
-        }
-
-        $date = date('Y-m-d');
-        $file = realpath(self::$dir) . DIRECTORY_SEPARATOR . "{$date}.log";
-
-        $args = func_get_args();
-
-        $output = [];
-        $time = date("H:i:s");
-        foreach ($args as $arg) {
-            $content = var_export($arg, true);
-            $output[] = "$time $content\n";
-        }
-
-        $result = file_put_contents($file, implode('', $output), FILE_APPEND + LOCK_EX);
-
-        return ($result === false) ? false : true;
     }
 
 
-    public static function writeTo($file, $var)
+    /**
+     * 任意级别。
+     * Logs with an arbitrary level.
+     *
+     * @param mixed  $level
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     *
+     * @throws InvalidArgumentException
+     */
+    public static function log($level, $message, array $context = [])
     {
-        $num_args = func_num_args();
-        if (!$num_args) {
-            return false;
+        // 如果尚未设置指定级别的log处理器，则什么都不做
+        if (static::$handlers[$level] === null) {
+            return;
         }
 
-        if (self::$dir === null || !file_exists(self::$dir) || !is_dir(self::$dir)) {
-            return false;
+        // 否则，调用设定的处理函数
+        call_user_func(static::$handlers[$level], $message, $context);
+    }
+
+
+    /**
+     * System is unusable.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public static function emergency($message, array $context = [])
+    {
+        if (static::$handlers[__FUNCTION__]) {
+            call_user_func(static::$handlers[__FUNCTION__], $message, $context);
         }
+    }
 
-        $target = realpath(self::$dir) . DIRECTORY_SEPARATOR . "$file";
 
-        $args = func_get_args();
-
-        $output = [];
-        $time = date("H:i:s");
-        for ($i = 1; $i < $num_args; $i++) {
-            $arg = $args[$i];
-            $content = var_export($arg, true);
-            $output[] = "$time $content\n";
+    /**
+     * critical
+     * Action must be taken immediately.
+     *
+     * Example: Entire website down, database unavailable, etc. This should
+     * trigger the SMS alerts and wake you up.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public static function alert($message, array $context = [])
+    {
+        if (static::$handlers[__FUNCTION__]) {
+            call_user_func(static::$handlers[__FUNCTION__], $message, $context);
         }
+    }
 
-        $result = file_put_contents($target, implode('', $output), FILE_APPEND + LOCK_EX);
 
-        return ($result === false) ? false : true;
+    /**
+     * 处理critical级日志。
+     * Critical conditions.
+     *
+     * Example: Application component unavailable, unexpected exception.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public static function critical($message, array $context = [])
+    {
+        if (static::$handlers[__FUNCTION__]) {
+            call_user_func(static::$handlers[__FUNCTION__], $message, $context);
+        }
+    }
+
+
+    /**
+     * 处理error级日志。
+     * Runtime errors that do not require immediate action but should typically
+     * be logged and monitored.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public static function error($message, array $context = [])
+    {
+        if (static::$handlers[__FUNCTION__]) {
+            call_user_func(static::$handlers[__FUNCTION__], $message, $context);
+        }
+    }
+
+
+    /**
+     * 处理warning级日志。
+     * Exceptional occurrences that are not errors.
+     *
+     * Example: Use of deprecated APIs, poor use of an API, undesirable things
+     * that are not necessarily wrong.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public static function warning($message, array $context = [])
+    {
+        if (static::$handlers[__FUNCTION__]) {
+            call_user_func(static::$handlers[__FUNCTION__], $message, $context);
+        }
+    }
+
+
+    /**
+     * 处理notice级日志。
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public static function notice($message, array $context = [])
+    {
+        if (static::$handlers[__FUNCTION__]) {
+            call_user_func(static::$handlers[__FUNCTION__], $message, $context);
+        }
+    }
+
+
+    /**
+     * 处理info级日志。
+     *
+     * Example: User logs in, SQL logs.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public static function info($message, array $context = [])
+    {
+        if (static::$handlers[__FUNCTION__]) {
+            call_user_func(static::$handlers[__FUNCTION__], $message, $context);
+        }
+    }
+
+
+    /**
+     * 处理debug级日志。
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public static function debug($message, array $context = [])
+    {
+        if (static::$handlers[__FUNCTION__]) {
+            call_user_func(static::$handlers[__FUNCTION__], $message, $context);
+        }
     }
 }
